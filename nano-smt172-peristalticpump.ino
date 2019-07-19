1b2e74d5be2d4ea5fd46e3c4968d648861d0701a
norbertk <norbertk@lavaleriana.net>
2019-07-19 10:18:10 +0200

    added positioning

--- nano-smt172-peristalticpump.ino
+++ nano-smt172-peristalticpump.ino
@@ -20,7 +20,9 @@ String str;
 int iCount, i;
 String sParams[6];
 bool accset = false;
-bool maxset = false;
+bool rotating =false;
+bool positioning = false;
+
 
 void setup() {
   Serial.begin(115200);
@@ -33,13 +35,15 @@ void loop() {
   if((time_now - last_update >= 1000) && (temp != 0)){
         Serial.println(temp);
         temp = 0;
-        stepper.runSpeed();
+        if(rotating){stepper.runSpeed();}
+        if(positioning){stepper.run();}
         last_update = millis();
   }
 
   if(time_now - last_measured >= 500){
     getTemp();
-    stepper.runSpeed();
+    if(rotating){stepper.runSpeed();}
+    if(positioning){stepper.run();}
     last_measured = millis();
   }
 
@@ -48,24 +52,29 @@ void loop() {
      iCount = StringSplit(str,':',sParams,6);
 
      //rotate at certain speed with certain acceleration.
-     if(sParams[0] == "rot"){
-        if(accset && maxset){
-          stepper.setSpeed(sParams[1].toFloat());        
-        }
-     }
-     
-     if(sParams[0] == "max" && sParams[1].toFloat() > 0){
-        stepper.setMaxSpeed(sParams[1].toFloat());
-        maxset = true;
+     if(sParams[0] == "rot" && accset){
+          rotating = true;
+          positioning = false;
+          stepper.setMaxSpeed(sParams[1].toFloat());
+          stepper.setSpeed(sParams[1].toFloat());
      }
 
      if(sParams[0] == "acc" && sParams[1].toFloat() > 0){
         stepper.setAcceleration(sParams[1].toFloat());
         accset = true;
      }
+
+     if(sParams[0] == "pos"){
+        positioning = true;
+        rotating = false;
+        stepper.setMaxSpeed(sParams[1].toFloat());
+        stepper.setSpeed(sParams[1].toFloat());
+        stepper.move(sParams[2].toInt());
+     }
      
   }
-  stepper.runSpeed();
+  if(rotating){stepper.runSpeed();}
+  if(positioning){stepper.run();}
 }
 
 
@@ -73,14 +82,19 @@ void getTemp(){
   SMT172::startTemperature(0.001);
   repeat:
   switch (SMT172::getStatus()) {
-    case 0: stepper.runSpeed();goto repeat;
+    case 0: 
+      if(rotating){stepper.runSpeed();}
+      if(positioning){stepper.run();}
+      goto repeat;
     case 1: 
       temp = SMT172::getTemperature();
-      stepper.runSpeed();
+      if(rotating){stepper.runSpeed();}
+      if(positioning){stepper.run();}
       break;
     case 2:
       Serial.println("no sensor");
-      stepper.runSpeed();
+      if(rotating){stepper.runSpeed();}
+      if(positioning){stepper.run();}
   }
 }
 
